a1492004101b63835b2c0ca66f389018dd1ba9de
Nur Akmal Jalil <48422687+nurakmaljalil91@users.noreply.github.com>
2025-11-06 06:38:25 +0800

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,17 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('cerxos-shell');
+  // remoteComponent?: Type<unknown>;
+  // constructor(private cdr: ChangeDetectorRef) { }
+
+  // async ngOnInit(): Promise<void> {
+  //   const module = await loadRemoteModule({
+  //     type: 'module',
+  //     remoteEntry: 'http://localhost:4201/remoteEntry.js',
+  //     exposedModule: './RemoteComponent'
+  //   });
+  //   console.log(Object.keys(module));
+  //   this.remoteComponent = (module as any).App;
+  //   this.cdr.markForCheck();
+  // }
 }
