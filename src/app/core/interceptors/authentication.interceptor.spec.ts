6d4ce6ff50f5db36a14f59cdd035c3942848f636
Nur Akmal Jalil <48422687+nurakmaljalil91@users.noreply.github.com>
2025-11-14 22:51:57 +0800

    Add tests for authentication interceptor with TokenService
    Expanded the authenticationInterceptor tests to verify Authorization header behavior when a token is present or null. Introduced a TokenService spy and improved test coverage for the interceptor logic.

--- src/app/core/interceptors/authentication.interceptor.spec.ts
+++ src/app/core/interceptors/authentication.interceptor.spec.ts
@@ -1,17 +1,56 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
-
+import { HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { provideZonelessChangeDetection } from '@angular/core';
 import { authenticationInterceptor } from './authentication.interceptor';
+import { TokenService } from '../services/token.service';
+import { of } from 'rxjs';
 
 describe('authenticationInterceptor', () => {
   const interceptor: HttpInterceptorFn = (req, next) =>
     TestBed.runInInjectionContext(() => authenticationInterceptor(req, next));
 
+  let tokenServiceSpy: jasmine.SpyObj<TokenService>;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    tokenServiceSpy = jasmine.createSpyObj(TokenService, ['get']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        provideZonelessChangeDetection(),
+        { provide: TokenService, useValue: tokenServiceSpy }
+      ]
+    });
   });
 
-  it('should be created', () => {
-    expect(interceptor).toBeTruthy();
+  it('should add Authorization header when token exists', (done) => {
+    tokenServiceSpy.get.and.returnValue('test-token');
+
+    const mockRequest = new HttpRequest('GET', '/test');
+
+    const next: HttpHandlerFn = (req: HttpRequest<unknown>) => {
+      expect(req.headers.has('Authorization')).toBeTrue();
+      expect(req.headers.get('Authorization')).toBe('Bearer test-token');
+      return of({} as HttpEvent<unknown>);
+    };
+
+    interceptor(mockRequest, next).subscribe(() => {
+      done();
+    });
+  });
+
+  it('should still call next when token service returns null', (done) => {
+    tokenServiceSpy.get.and.returnValue(null);
+
+    const mockRequest = new HttpRequest('GET', '/test');
+
+    const next: HttpHandlerFn = (req: HttpRequest<unknown>) => {
+      expect(req.headers.get('Authorization')).toBe('Bearer null');
+      return of({} as HttpEvent<unknown>);
+    };
+
+    interceptor(mockRequest, next).subscribe(() => {
+      done();
+    });
   });
 });
